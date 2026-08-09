ccf7a5282e612f51f6929df8dcd80271cd884007
Hawk Studio <magk34229@gmail.com>
2026-08-09 13:18:34 +0200

    Start
    1

---
+++ script.js
@@ -0,0 +1,25 @@
+fetch("games.json")
+    .then(response => response.json())
+    .then(games => {
+
+        const container = document.getElementById("games-container");
+
+        games.forEach(game => {
+
+            const tile = document.createElement("div");
+
+            tile.classList.add("game");
+
+            tile.dataset.id = game.id;
+
+            tile.innerHTML = `
+                <a href="${game.link}">
+                    <img src="${game.image}" alt="${game.title}">
+                </a>
+                <p>${game.title}</p>
+            `;
+
+            container.appendChild(tile);
+        });
+
+    });

1b3f084ff54e69b2f11a9d171aa5757333f1c3d0
Hawk Studio <magk34229@gmail.com>
2026-08-13 15:16:59 +0200

    Revolution

--- game/catzstudio.js
+++
@@ -1,5 +0,0 @@
-fetch("catzstudio.md")
-    .then(response => response.text())
-    .then(markdown => {
-        document.getElementById("catzstudio-article").innerHTML = marked.parse(markdown);
-    });
